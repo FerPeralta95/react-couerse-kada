5cb5c7a134a77bbbcac94d8e9c2454ad91e7514e
FerPeralta <ferperalta.kadasoftware@gmail.com>
2023-05-20 16:45:49 -0600

    Se agregaron todas las extensiones

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Curso de React
+
 Comandos iniciales:
 En la teminal de Linux:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-"Cerrrar y volver a abrir la terminal"
+"Cerrar y volver a abrir la terminal"
 code
 
 "En la terminal de VSC"
-npm init -y 
+npm init -y
 
 Instalar todas las dependencias en los package.json y package-lock.json
 Se agregan todos los configs
-
 
 ## Once everything installed, you can run:
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
 //Dependencies
 import { createRoot } from 'react-dom/client';
 
+//Components
+import { Pet } from './Pet';
+
+const animales = [
+  {
+    name: 'Max',
+    type: 'Perro Boxer',
+    breed: 'Croquetas',
+  },
+  {
+    name: 'Tommy',
+    type: 'Perrito Zchanauzer',
+    breed: 'Pollito',
+  },
+  {
+    name: 'Odin',
+    type: 'Loro',
+    breed: 'Semillas',
+  },
+  {
+    name: 'Nemo',
+    type: 'Pez Payaso',
+    breed: 'Comida de pez',
+  },
+  {
+    name: 'Simba',
+    type: 'León',
+    breed: 'Carne',
+  },
+];
+
 const App = () => (
-    <div>
-        <h1>Hello World</h1>
-    </div>
+  <div>
+    <Pet animal={animales[0]} />
+    <Pet animal={animales[1]} />
+    <Pet animal={animales[2]} />
+    <Pet animal={animales[3]} />
+    <Pet animal={animales[4]} />
+  </div>
 );
 
 const container = document.getElementById('root');
