7ea406a9ac1d1a68831d052d0ee25e1770fd42ec
FerPeralta <ferperalta.kadasoftware@gmail.com>
2023-05-27 13:14:30 -0600

    Se agrega uso de hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,12 @@
 import { createRoot } from 'react-dom/client';
 
 //Components
-import { Pet } from './Pet';
-
-const animales = [
-  {
-    name: 'Max',
-    type: 'Perro Boxer',
-    breed: 'Croquetas',
-  },
-  {
-    name: 'Tommy',
-    type: 'Perrito Zchanauzer',
-    breed: 'Pollito',
-  },
-  {
-    name: 'Odin',
-    type: 'Loro',
-    breed: 'Semillas',
-  },
-  {
-    name: 'Nemo',
-    type: 'Pez Payaso',
-    breed: 'Comida de pez',
-  },
-  {
-    name: 'Simba',
-    type: 'León',
-    breed: 'Carne',
-  },
-];
+import { SearchParams } from './components/SearchParams';
 
 const App = () => (
   <div>
-    <Pet animal={animales[0]} />
-    <Pet animal={animales[1]} />
-    <Pet animal={animales[2]} />
-    <Pet animal={animales[3]} />
-    <Pet animal={animales[4]} />
+    <h1>Adopt me!</h1>
+    <SearchParams />
   </div>
 );
 
